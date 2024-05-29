9e5e65505cecc92cc53f8f911dd089bdc58685fe
Marcus Becker <exoticboymarcus@gmail.com>
2024-05-29 12:12:52 +0200

    JS refactored/ Button Prev and Next refactored

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,52 +1,80 @@
-// Schritt 1: DOM-Elemente abrufen
-let nextDom = document.getElementById('next');
-let prevDom = document.getElementById('prev');
+class Carousel {
+    constructor(carouselSelector, timeRunning = 700, timeAutoNext = 6000) {
+        this.carouselDom = document.querySelector(carouselSelector);
+        this.nextDom = this.carouselDom.querySelector('#next');
+        this.prevDom = this.carouselDom.querySelector('#prev');
+        this.sliderDom = this.carouselDom.querySelector('.list');
+        this.thumbnailBorderDom = this.carouselDom.querySelector('.thumbnail-container .thumbnail');
+        this.timeDom = this.carouselDom.querySelector('.time');
+        this.timeRunning = timeRunning;
+        this.timeAutoNext = timeAutoNext;
 
-let carouselDom = document.querySelector('.carousel');
-let SliderDom = carouselDom.querySelector('.carousel .list');
-let thumbnailBorderDom = document.querySelector('.carousel .thumbnail-container .thumbnail');
-let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
-let timeDom = document.querySelector('.carousel .time');
-
-thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-let timeRunning = 1000; // Reduzierte Zeit für Animationen
-let timeAutoNext = 6000;
-
-nextDom.onclick = function () {
-    showSlider('next');
-}
-
-prevDom.onclick = function () {
-    showSlider('prev');
-}
-
-let runTimeOut;
-let runNextAuto = setTimeout(() => {
-    nextDom.click();
-}, timeAutoNext);
-
-function showSlider(type) {
-    let SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
-    let thumbnailItemsDom = document.querySelectorAll('.thumbnail-container .thumbnail .item');
-
-    if (type === 'next') {
-        SliderDom.appendChild(SliderItemsDom[0]);
-        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-        carouselDom.classList.add('next');
-    } else {
-        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
-        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-        carouselDom.classList.add('prev');
+        this.init();
     }
 
-    clearTimeout(runTimeOut);
-    runTimeOut = setTimeout(() => {
-        carouselDom.classList.remove('next');
-        carouselDom.classList.remove('prev');
-    }, timeRunning);
+    init() {
+        this.thumbnailItemsDom = this.thumbnailBorderDom.querySelectorAll('.item');
+        this.thumbnailBorderDom.appendChild(this.thumbnailItemsDom[0]);
 
-    clearTimeout(runNextAuto);
-    runNextAuto = setTimeout(() => {
-        nextDom.click();
-    }, timeAutoNext);
+        this.nextDom.onclick = () => this.showSlider('next');
+        this.prevDom.onclick = () => this.showSlider('prev');
+
+        this.runNextAuto = setTimeout(() => {
+            this.nextDom.click();
+        }, this.timeAutoNext);
+
+        this.resetTimeBar();
+    }
+
+    showSlider(type) {
+        this.disableButtons();
+
+        const sliderItemsDom = this.sliderDom.querySelectorAll('.item');
+        const thumbnailItemsDom = this.thumbnailBorderDom.querySelectorAll('.item');
+
+        if (type === 'next') {
+            this.sliderDom.appendChild(sliderItemsDom[0]);
+            this.thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+            this.carouselDom.classList.add('next');
+        } else {
+            this.sliderDom.prepend(sliderItemsDom[sliderItemsDom.length - 1]);
+            this.thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+            this.carouselDom.classList.add('prev');
+        }
+
+        clearTimeout(this.runTimeout);
+        this.runTimeout = setTimeout(() => {
+            this.carouselDom.classList.remove('next');
+            this.carouselDom.classList.remove('prev');
+            this.enableButtons();
+        }, this.timeRunning);
+
+        clearTimeout(this.runNextAuto);
+        this.runNextAuto = setTimeout(() => {
+            this.nextDom.click();
+        }, this.timeAutoNext);
+
+        this.resetTimeBar();
+    }
+
+    disableButtons() {
+        this.nextDom.disabled = true;
+        this.prevDom.disabled = true;
+    }
+
+    enableButtons() {
+        this.nextDom.disabled = false;
+        this.prevDom.disabled = false;
+    }
+
+    resetTimeBar() {
+        this.timeDom.style.animation = 'none';
+        this.timeDom.offsetHeight; // Trigger reflow to restart the animation
+        this.timeDom.style.animation = `runningTime ${this.timeAutoNext / 1000}s linear 1 forwards`;
+    }
 }
+
+// Initialize the carousel
+document.addEventListener('DOMContentLoaded', () => {
+    new Carousel('.carousel');
+});
